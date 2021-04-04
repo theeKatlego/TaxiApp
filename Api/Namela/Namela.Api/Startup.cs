9b0556ee9f76f4bb4adce075eee446e1135213f5
kseletisha@oldmutual.com <kseletisha@oldmutual.com>
2021-04-05 00:18:44 +0200

    Added api project - azure function project.

---
+++ Api/Namela/Namela.Api/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.DependencyInjection;
+using Namela.Infrastructure;
+using Namela.Infrastructure.DependencyInjection;
+using Namela.Api;
+using Namela.Domain.Features.Rides;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace Namela.Api
+{
+    public class Startup: FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var configuration = builder.GetContext().Configuration;
+
+            var storageConnectionString = configuration["StorageConnectionString"];
+            var cosmosDbEndpointUri = configuration["CosmosDbEndpointUri"];
+            var cosmosDbPrimaryKey = configuration["CosmosDbPrimaryKey"];
+            var applicationAssembly = typeof(RequestCommand).Assembly;
+
+            builder.Services.AddTransient<IFunctionFilter, DatabaseTransactionFilter>();
+
+            builder.Services.AddApplication(applicationAssembly);
+
+            builder.Services.AddInfrastructure(
+                storageConnectionString: storageConnectionString,
+                cosmosDbEndpointUri: cosmosDbEndpointUri,
+                cosmosDbPrimaryKey: cosmosDbPrimaryKey
+            );
+        }
+    }
+}
---
+++ Api/Namela/Namela.Domain/Features/Rides/Request.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Namela.Domain.Features.Rides
+{
+    public class RequestCommand: IRequest
+    {
+        
+    }
+    
+    public class RequestHandler: IRequestHandler<RequestCommand>
+    {
+        public Task<Unit> Handle(RequestCommand request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
